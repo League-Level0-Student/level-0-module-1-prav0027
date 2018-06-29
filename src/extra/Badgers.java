fa7be08d738a1505142ce709e2e5025b0ae6f2de
prav0027 <league@WTS22.attlocal.net>
2018-06-28 20:06:03 -0700

    Badgers

---
+++ src/extra/Badgers.java
@@ -0,0 +1,20 @@
+package extra;
+
+public class Badgers {
+	public static void main(String[] args) {
+		for (int i = 0; i < 3; i++) {
+			for (int x = 0; x < 12; x++) {
+				System.out.println("Badger, ");
+			}
+			for (int a = 0; a < 2; a++) {
+				System.out.println("Mushroom, ");
+			}
+		}
+		for (int b = 0; b < 11; b++) {
+			System.out.println("Badger, ");
+		}
+		for (int c = 0; c < 5; c++) {
+			System.out.println("Snake, ");
+		}
+	}
+}
